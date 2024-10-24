f6b22c00e2945f07f14c839eb6f78eb27091151a
Ahmed Yousri <ahmedyousri680@outlook.com>
2024-10-25 01:18:55 +0300

    Docker test

--- backend/bracket/config.py
+++ backend/bracket/config.py
@@ -38,7 +38,7 @@ class Config(BaseSettings):
     cors_origins: str = "*"
     jwt_secret: str
     auto_run_migrations: bool = True
-    pg_dsn: PostgresDsn = "postgresql://bracket_dev:bracket_dev@localhost:5432/bracket_dev"  # type: ignore[assignment]
+    pg_dsn: PostgresDsn = "postgresql://hhaggan:PassDemo%40123@f9-pstgrs-srver.postgres.database.azure.com:5432/postgres"  # type: ignore[assignment]
     sentry_dsn: str | None = None
 
     def is_cors_enabled(self) -> bool:
